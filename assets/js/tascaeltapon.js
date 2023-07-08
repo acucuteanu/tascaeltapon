a3c8be9d0134a9e0dc576324ec758dd76095e1c6
acucuteanu <acucuteanu010@gmail.com>
2023-07-08 05:47:49 +0200

    Terminado PC

--- assets/js/tascaeltapon.js
+++ assets/js/tascaeltapon.js
@@ -1,2 +1,17 @@
+/////////////////////////////////
+//		BOTÓN IR ARRIBA		  //
+////////////////////////////////
 
-console.log("hola")
+$('.ir-arriba').click(function(){
+	$('body, html').animate({
+		scrollTop: '0px'
+	}, 300);
+});
+
+$(window).scroll(function(){
+	if( $(this).scrollTop() > 0 ){
+		$('.ir-arriba').slideDown(300);
+	} else {
+		$('.ir-arriba').slideUp(300);
+	}
+});
